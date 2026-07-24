9dfa16c48fb2e8119aba1cb9241c1f460c43e217
fora2323 <fora060823@gmail.com>
2026-07-24 19:16:22 +0800

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aexon"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2017
+        versionName = "1.3"
      }
 
     buildFeatures { viewBinding = true }
